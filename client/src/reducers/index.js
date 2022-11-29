6eec25864bd87cc414d9442a5b7e067d9a0ac0d8
Artyom Nazarov <artyom.nazaroff@yandex.ru>
2022-11-29 09:44:48 +0400

    Add redux

---
+++ client/src/reducers/index.js
@@ -0,0 +1,12 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import userReducer from './userReducer';
+import fileReducer from './fileReducer';
+
+const rootReducer = combineReducers({
+	user: userReducer,
+	file: fileReducer,
+})
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
